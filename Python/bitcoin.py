ab67af9b994eba1f6df3d44bd231fe4517881695
Capita1 <capita1_gabriel@outlook.com>
2024-08-19 13:26:50 -0300

    bitcoin

--- Python/bitcoin.py
+++ Python/bitcoin.py
@@ -4,9 +4,12 @@ matplotlib.use('module://matplotlib-backend-kitty')
 import matplotlib.pyplot as plt
 import pandas as pd 
 import urllib.request, json 
+import time, datetime
 
-data = json.load(urllib.request.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json"))
-valor = (data['bpi']['USD']['rate'])
-
-print(valor)
-
+while (True):
+    data = json.load(urllib.request.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json"))
+    h = (datetime.datetime.now().time())
+    h = str(h)
+    print(f'{h[:8]} -> ${data['bpi']['USD']['rate']}') 
+    time.sleep(30)
+        
