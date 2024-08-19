dde9867f1841ef1dd5071d90d95b94d083aadee9
Capita1 <capita1_gabriel@outlook.com>
2024-08-19 14:12:12 -0300

    Quase funciona

--- Python/bitcoin.py
+++ Python/bitcoin.py
@@ -3,13 +3,35 @@ import matplotlib
 matplotlib.use('module://matplotlib-backend-kitty')
 import matplotlib.pyplot as plt
 import pandas as pd 
-import urllib.request, json 
-import time, datetime
+import numpy as np
+import urllib.request, json, time, datetime
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 
-while (True):
-    data = json.load(urllib.request.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json"))
-    h = (datetime.datetime.now().time())
-    h = str(h)
-    print(f'{h[:8]} -> ${data['bpi']['USD']['rate']}') 
-    time.sleep(30)
-        
+data = json.load(urllib.request.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json"))
+h = str(datetime.datetime.now().time())
+btc = data['bpi']['USD']['rate']
+print(f'{h[:8]} -> ${btc}') 
+      
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
+
+def animate(i, xs, ys):
+    xs.append(h)
+    ys.append(btc)
+
+    xs = xs[-20:]
+    ys = ys[-20:]
+
+    ax.clear()
+    ax.plot(xs, ys)
+
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('BTC')
+    plt.ylabel('Preço')
+
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+plt.show()
