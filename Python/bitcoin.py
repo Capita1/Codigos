dc8152a1b610df5504c5d06606cfa25e6198e7b0
Capita1 <capita1_gabriel@outlook.com>
2024-08-22 16:21:19 -0300

    Bitcoin.py

--- Python/bitcoin.py
+++ Python/bitcoin.py
@@ -2,24 +2,31 @@
 import matplotlib
 matplotlib.use('module://matplotlib-backend-kitty')
 import matplotlib.pyplot as plt
+import numpy as np
 import urllib.request, json, time, datetime
 
 delay=30
 x = []
 y = []
+loop = True
 
-while(True):
+while(loop):
     data = json.load(urllib.request.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json"))
     h = (str(datetime.datetime.now().time()))[:8]
-    btc = data['bpi']['USD']['rate']
+    btc = float((data['bpi']['USD']['rate'])[:2])*1000+float((data['bpi']['USD']['rate'])[3:])
     print(f'{h} -> ${btc}') 
     x.append(h)
     y.append(btc)
-    
     for s in range(delay):
         time.sleep(1)
-        print(f'\t[{s}]',end='\r')
-    if (len(x)==5):
-        plt.plot(x,y)
-        plt.show()
+        print(f'\t[{30-s}]',end='\r')
 
+    if (x == 5):
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set(xlabel='hora', ylabel='USD',
+               title='Bitcoin')
+        ax.grid()
+
+        fig.savefig("test.png")
+        plt.show()
